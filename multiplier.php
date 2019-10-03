cd56a98b3f717f8285cb1351d1f4d5af5fea2b85
danieljunek15 <dvandenbrink@roc-dev.com>
2019-10-03 20:17:21 +0200

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,7 @@
+<?php
+echo "gello my name is daniel i just learned how to multipli i must give me one number you like" .PHP_EOL;
+$firstNumber=readline();
+echo "nice now me like second number you give me ?" .PHP_EOL;
+$secondNumber=readline();
+$awnser=($firstNumber * $secondNumber);
+echo "ik kan net rekenen en hier heb je je antwoord: " . " " . "$awnser";
